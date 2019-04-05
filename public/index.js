aabeeb37146aae0cde75fc99b3d891b2931dafdd
pramjoshcyb <45024617+pramjoshcyb@users.noreply.github.com>
2019-04-04 22:35:53 -0400

    added dummy data to index.js file

---
+++ public/index.js
@@ -0,0 +1,62 @@
+const logs = [
+    {
+        id:         1,
+        severity:   "low",
+        reportType: "script",
+        timestamp:  15243627
+    },
+    {
+        id:         2,
+        severity:   "critical",
+        reportType: "mixed content",
+        timestamp:  038317927
+    },
+    {
+        id:         3,
+        severity:   "moderate",
+        reportType: "style-src",
+        timestamp:  1554431444,
+    },
+    {
+        id:         4,
+        severity:   "severe",
+        reportType: "script-src",
+        timestamp:  1554431448,
+    },
+    {
+        id:         5,
+        severity:   "High",
+        reportType: "Violated Directive",
+        timestamp:  1554431256
+    },
+    {
+        id:         6,
+        severity:   "Medium",
+        reportType: "Mixed Content",
+        timestamp:  1554431278
+    },
+    {
+        id:         7,
+        severity:   "high",
+        reportType: "script",
+        timestamp:  15649483904
+    }, 
+    {
+        id:         8,
+        severity:   "low",
+        reportType: "mixed content",
+        timestamp:  15649483905
+    },
+    {
+        id:         9,
+        severity:   "medium",
+        reportType: "script",
+        timestamp:  1554431249
+    },
+    {
+        id:         10,
+        severity:   "high",
+        reportType: "style",
+        timestamp:  1554431375
+    }
+]

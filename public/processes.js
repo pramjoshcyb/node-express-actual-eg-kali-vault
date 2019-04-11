8bb912997e91267268fc59588f18cd0bade04fd3
pramjoshcyb <45024617+pramjoshcyb@users.noreply.github.com>
2019-04-10 23:48:38 -0400

    fork examples

--- public/processes.js
+++ public/processes.js
@@ -1,4 +1,6 @@
 const child_process = require('child process')
-child_process.fork('./processes'); //fork bomb, takes up resources until the system is killed, infinite loop
 
+child_process.fork('./forkee'); //fork bomb, takes up resources until the system is killed, infinite loop
+
+// can check number of processes in system by issuing ps command
 

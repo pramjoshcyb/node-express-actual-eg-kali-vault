8cbe3f547879e251f3a20bc6e1e1da4030331bac
pramjoshcyb <45024617+pramjoshcyb@users.noreply.github.com>
2019-04-12 01:34:05 -0400

    added new file checkfilesizeprocess.js which takes in regular expressions and checks file size from a new process

--- check-filesize-process.js
+++ check-filesize-process.js
@@ -12,3 +12,21 @@ const secondcmd = "ls -lt reports*.log | sed 's/\\([^ ]* *\\)\\{4\\}\\[^ ]*\\).*
 // finds the four groups of characters which are lead by spaces (manually), and replaces line with the next group
 
 const thirdcmd = "ls -lt reports*.log | sed 's/[^ ]* *[^ ]* *[^ ]* *[^ ]* *[^ ]* *\\([^ ]*\\).*/\\1/' | cut -d ' ' -f 5 | head -n 1";
+
+
+
+module.exports = function () { // JavaScript module.exports which is a unique object included in all the JavaScript files in Node.js framework by default. 
+// module is a variable representing the existing module
+// exports is an object exposed to the module 
+
+// child_process.exec spawns a child process from the child_process file
+    const child = child_process.exec(cmd1, 
+        (error, stdout, stderr) => { // parameters passed in containing error, standard out and standard error file descriptor
+            /* if (error) { // can something go wrong?
+                console.error(`exec error: ${error}`);
+                return;
+            }*/
+            console.log(`File size: ${stdout}`);
+            //console.log(`stderr: ${stderr}`);
+        }); 
+}

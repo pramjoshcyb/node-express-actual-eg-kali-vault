1780849cc4e912892a7f9968f312637727571ec5
pramjoshcyb <45024617+pramjoshcyb@users.noreply.github.com>
2019-04-15 09:51:16 -0400

    added test cases and lookup methods to look up existent and non existent entries in BST

--- bst.test.js
+++ bst.test.js
@@ -19,7 +19,25 @@ describe("test the lookup method in trivial trees", () => {
         expect(t.lookup)("groot")).toBe(55);
         expect(t.lookup(5)).toBe(null);
     });
+});
 
+describe("test add and lookup methods", () => {
+
+    const t = new BST();
+
+    const toAdd = [50, 25, 12, 6, 37, 31, 75, 66, 89, 95];
+    for (let i = 0; i < toAdd.length; i++) {
+        t.insert(toAdd[i], 'test ' + toAdd[i]);
+    }
+    test("lookup existent and non-existent entries in bst", () => {
+        for (let i = 0; i < 100; i++) {
+            if (toAdd.indexOf(i) === -1) {
+                expect(t.lookup(i)).toBe(null);
+            } else {
+                expect(t.lookup(i)).toBe('test ' + i);
+            }
+        }
+    });
 });
 
 

239838c34b093964947b85928f922713885bfda5
pramjoshcyb <45024617+pramjoshcyb@users.noreply.github.com>
2019-04-15 09:36:35 -0400

    test item 1 added

--- bst.test.js
+++ bst.test.js
@@ -1,1 +1,25 @@
-const BST = require('./bst'); //gets the bst.js file linked
+const BST = require('./bst'); //gets the bst.js file from the public folder
+// test file for bst.js
+// reference video: https://www.youtube.com/watch?v=_Zcgy_8JEaI&list=PLj_wfJztgvMIMSwtsC0r2f8wDZugQPcNF&index=69
+
+
+//trivial tree is a tree with 0 or 1 items in it
+describe("test the lookup method in trivial trees", () => {
+
+    const t = new BST();
+
+    // first test item would be null since it starts at 0 
+    test("lookup in empty list should return null", () => {
+        expect(t.lookup("groot")).toBe(null);
+        expect(t.lookup(5)).toBe(null);
+    });
+
+    test("lookup in a list with one entry", () => {
+        t.insert('groot', 55);
+        expect(t.lookup)("groot")).toBe(55);
+        expect(t.lookup(5)).toBe(null);
+    });
+
+});
+
+
